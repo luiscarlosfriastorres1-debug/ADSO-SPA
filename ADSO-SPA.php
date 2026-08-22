1272b967c36429ffa8c7286306c3358aab2dea5b
Luisca <luiscarlosfriastorres1@gmail.com>
2026-08-22 18:03:15 -0500

    OP 2 y 1 Complete sin Validaciones

--- ADSO-SPA.php
+++ ADSO-SPA.php
@@ -1,12 +1,46 @@
 <?php
+
+
+// Listas y variables de mi proyecto
+
+
 $empleados = [];
+$citas = [];
+$dias_de_la_semana = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado"];
+
+
 $op_activas = true;
 $op_activas_register = true;
 $i = 0;
+
+$servicios = [
+    ["nombre" => "Limpieza facial",           "precio" => 80000,  "duracion" => 2],
+    ["nombre" => "Manicure",                  "precio" => 35000,  "duracion" => 1],
+    ["nombre" => "Pedicure",                  "precio" => 40000,  "duracion" => 1],
+    ["nombre" => "Masaje relajante",          "precio" => 90000,  "duracion" => 1],
+    ["nombre" => "Masaje descontracturante",  "precio" => 100000, "duracion" => 1],
+    ["nombre" => "Exfoliación corporal",      "precio" => 60000,  "duracion" => 1],
+    ["nombre" => "Tratamiento antiedad",      "precio" => 120000, "duracion" => 2],
+];
+
+
+
+// Funciones de mi proyecto
+
+
 function registrarEmpleado($empleados){
     $nombre = readline("Ingrese el nombre del empleado: ");
+	
+	// Valido para que el nombre no este vacio
+	while ($nombre == "") {
+	
+		$nombre = readline("No puede estar vacio. Ingrese el nombre del empleado: ");
+	
+	}
     $cargo = readline("Ingrese el cargo del empleado: ");
-
+	while ($cargo == ""){
+		$cargo = readline("No puede estar vacio. Ingrese el cargo del empleado: ");
+	}
     $empleados[] = [
 		"id" => count($empleados) + 1,
         "nombre" => $nombre,
@@ -15,10 +49,86 @@ function registrarEmpleado($empleados){
 
     return $empleados;
 }
+// funciones para las citas 
+
+function mostrarCitas($citas){
+
+	if(count($citas) == 0){
+		echo "No hay citas registradas \n";
+	}
+	else{
+		foreach($citas as $cita){
+			echo "Empleado: " . $cita["idEmpleado"] . " Cliente: " . $cita["cliente"] . " Dia: " . $cita["dia"] . " Hora: " . $cita["hora"] . "\n";
+		}
+	}
+}
+
+function registrarCita($empleados, $citas, $servicios, $dias_de_la_semana){
+
+	if(count($empleados) == 0){
+
+		echo "No hay empleados registrados \n";
+
+	}
+	else{
+				
+		foreach($empleados as $emple){
+		
+			echo $emple["id"] . " " . "nombre: ". $emple["nombre"] . " cargo: " .$emple["cargo"] ."\n";
+		
+		}
+
+		$id_empleado = readline("Seleccione el numero del empleado: ");
+		$cliente = readline("Ingrese nombre del cliente: ");
+		
+		while($cliente == ""){
+		
+			$cliente = readline("No puede estar vacio. Ingrese el cliente: ");
+		
+		}
+
+		echo "Dias Disponibles: ";
+		foreach($dias_de_la_semana as $dia){
+			echo $dia . " ";
+		}
+		echo "\n";
+		$dia = readline("Ingrese el dia: ");
+		
+		while($dia != "lunes" && $dia != "martes" && $dia != "miercoles" && $dia != "jueves" && $dia != "viernes" && $dia != "sabado"){
+		
+			$dia = readline("Dia invalido. Ingrese el dia: ");
+		
+		}
+
+		$hora = readline("Ingrese la hora: ");
+		
+		while($hora < 8 || $hora > 18){
+		
+			$hora = readline("Hora invalida. Ingrese la hora: ");
+		
+		}
+
+		$citas[] = [
+			"idEmpleado" => $id_empleado,
+			"cliente" => $cliente,
+			"dia" => $dia,
+			"hora" => $hora
+		];
+
+		echo "Cita registrada \n";
+
+	}
+
+	return $citas;
+}
+
+
+
+////////////////////////////////////
 
 while($op_activas){
 	
-	echo "====== Bienvenido ADSO-SPA =======\n";
+	echo "====== Menu De ADSO-SPA =======\n";
 	echo " \n";
 	echo "| 1. Registrar empleado           |\n";
 	echo "| 2. Registrar cita               |\n";
@@ -63,14 +173,11 @@ while($op_activas){
 
 			break;
 		case 2:
-		
-			foreach($empleados as $emple){
-				echo $emple["id"] . " " . "nombre: ". $emple["nombre"] . " cargo: " .$emple["cargo"] ."\n";
-			}
+			$citas = registrarCita($empleados, $citas, $servicios, $dias_de_la_semana);
 
 			break;
 		case 3:
-
+			mostrarCitas($citas);
 
 			break;
 		case 4:
